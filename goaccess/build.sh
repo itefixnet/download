73a9cce81a9cf77de5db2e400984fffc5109521d
tevkar <tk@itefix.net>
2025-09-20 14:49:23 +0200

    goaccess build.sh

---
+++ goaccess/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+VERSION="goaccess-version"
+bs_workspace="folder"
+
+wget https://tar.goaccess.io/goaccess-${VERSION}.tar.gz
+tar xzvf goaccess-${VERSION}.tar.gz
+
+cd goaccess-${VERSION}
+./configure  --sysconfdir=/etc --enable-utf8 --enable-geoip=legacy --with-openssl
+make
+
+./goaccess.exe --version
+strip goaccess.exe
+
+cat goaccess.1 | groff -mandoc -Thtml >goaccess.html
+
+tar cvzf ../${bs_workspace}.tar.gz goaccess.exe goaccess.html config/*
